26f25209f81dc0a15ba57c422cebd6ddc6b0e4fc
shanajasila <shanajasilam@gmail.com>
2022-12-06 11:00:50 +0530

    view

--- src/main/java/com/example/courseapp/controller/CourseController.java
+++ src/main/java/com/example/courseapp/controller/CourseController.java
@@ -6,5 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class CourseController {
-
+    @GetMapping("/view")
+    public String viewcourse(){
+        return "Welcome to view page";
+    }
 }
